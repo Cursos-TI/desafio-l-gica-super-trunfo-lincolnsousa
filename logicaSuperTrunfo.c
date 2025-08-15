8b23cb111332904fc09eec42a5d8be7964347b99
lincolnsousa <lincolnreis1810@gmail.com>
2025-08-15 15:33:37 +0000

    Aplicação concluida

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -27,10 +27,53 @@ int main() {
     float densidade2 = populacao2 / area2;
     float perCapita2 = prod2 / populacao2;
 
-    if(prod > prod2) {
-        printf("A Carta 1 venceu!\n");
-    } else {
-        printf("A Carta 2 venceu!\n");
+    //Jogo
+
+    int opcao;
+
+    while (&opcao != 3){
+
+    
+    printf("Menu Principal\n");
+        printf("1. Comparar PIB e Densidade de Cartas das Cartas\n");
+        printf("2. Ver regras\n");
+        printf("3. Sair da aplicação\n");
+        scanf("%d", &opcao);
+    
+    
+
+        switch (opcao) {
+            case 1:
+                printf("Estados: %s, %s \n", estado, estado2);
+                printf("PIB e Densidade\n");
+                printf("Comparando PIB:\n");
+                    if(prod > prod2) {
+                        printf("Carta 1 venceu!\n");
+                    } else if (prod == prod2) {
+                        printf("Empate!\n");
+                    } else {
+                        printf("A carta 2 venceu!\n");
+                    }
+                printf("Comparando Densidade:\n");
+                    if(densidade > densidade2) {
+                        printf("Carta 2 venceu!\n");
+                    } else if (densidade == densidade2) {
+                        printf("Empate!\n");
+                    } else {
+                        printf("A carta 1 venceu!\n");
+                    }
+                break;
+            case 2:
+                printf("Na comparação a carta com maior PIB vence e a carta com menor Densidade vence\n");
+                break;
+            case 3:
+                printf("Saindo da aplicação\n");
+                break;
+            default:
+                printf("Essa opção não existe\n");
+                break;
+        }
+
     }
 
     return 0;
